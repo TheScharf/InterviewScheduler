062fed5f61e437a4bf2a8c6e5790437c11652308
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-23 19:08:06 -0600

    builds the interviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  let interviewerClass = classNames("interviewers", {
+    "interviewers__item--selected": props.selected
+  })
+  return(
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+    <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+    />
+    {props.selected && props.name}
+  </li>
+  )
+}
