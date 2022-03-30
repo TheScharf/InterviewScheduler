44d0fda953d9dee35a186930b6779376cc7fef8e
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-30 17:13:26 -0600

    incorporating tests and validation function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,8 @@ import Button from "components/Button";
 export default function Form(props){
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
   function reset() {
     setStudent("");
     setInterviewer(null);
@@ -13,6 +15,14 @@ export default function Form(props){
   function cancel() {
     props.onCancel();
     reset();
+  }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    props.onSave(student, interviewer);
   }
 
   return(
@@ -26,8 +36,10 @@ export default function Form(props){
         placeholder="Enter Student Name"
         value={student}
         onChange={(event) => setStudent(event.target.value)}
+        data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList 
       value={interviewer}
       interviewers={props.interviewers}
@@ -37,7 +49,7 @@ export default function Form(props){
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+      <Button confirm onClick={validate}>Save</Button>
     </section>
   </section>
 </main>
